2171b61c39d37f8c06309abd50f82a680209271a
ppuivif <papuivif@gmail.com>
2024-05-21 14:45:56 +0200

    2024-05-20

--- expand.c
+++ expand.c
@@ -2,48 +2,62 @@
 
 void	expand_contents(t_command_line **command_line)
 {
-	size_t	i;
-	size_t	j;
+	//size_t	i;
+	//size_t	j;
 	t_substring	*tmp1;
 	t_native_redirection *tmp2;
 	t_native_argument *tmp3;
+	char *remaining_line;
 	//t_expanded_redirection	*exp_redirection;
 	//t_expanded_argument		*exp_argument;
 
-	i = 0;
+	//i = 0;
 	tmp1 = (*command_line)->substrings;
-	while ((*command_line)->substrings && i < ft_lst_size1((*command_line)->substrings))
+	remaining_line = NULL;
+	//while ((*command_line)->substrings && i < ft_lst_size1((*command_line)->substrings))
+	while (tmp1)
 	{
-		j = 0;
+	//	j = 0;
 		tmp2 = tmp1->n_redirections;
-		while (tmp1->n_redirections && j < ft_lst_size2(tmp1->n_redirections))
+	//	while (tmp1->n_redirections && j < ft_lst_size2(tmp1->n_redirections))
+		while (tmp2)
 		{
-			expand_redirections((*command_line)->substrings, tmp1->n_redirections);
-			if ((*command_line)->substrings->exp_redirections->alloc_succeed == false)
-				free_all(command_line);	
+//			expand_redirections((*command_line)->substrings, tmp1->n_redirections);
+			remaining_line = tmp2->content;
+			while (remaining_line)
+			{
+				expand_redirections(tmp1, tmp2, &remaining_line);
+				if (tmp1->exp_redirections->alloc_succeed == false)
+					free_all(command_line);	
 //			printf("exp_redirection %d : %s\n", exp_redirection->content);
+			}
 			tmp2 = tmp2->next;
-			j++;
+//			j++;
 		}
-		j = 0;
+//		j = 0;
 		tmp3 = tmp1->n_arguments;
-		while (tmp1->n_arguments && j < ft_lst_size3(tmp1->n_arguments))
+//		while (tmp1->n_arguments && j < ft_lst_size3(tmp1->n_arguments))
+		while (tmp3)
 		{
-			expand_arguments((*command_line)->substrings, tmp1->n_arguments);
-			if ((*command_line)->substrings->exp_arguments->alloc_succeed == false)
+//			expand_arguments((*command_line)->substrings, tmp1->n_arguments);
+			expand_arguments(tmp1, tmp3);
+//			if ((*command_line)->substrings->exp_arguments->alloc_succeed == false)
+			if (tmp1->exp_arguments->alloc_succeed == false)
 				free_all(command_line);
 			tmp3 = tmp3->next;
-			j++;
+//			j++;
 		}
-		(*command_line)->substrings = (*command_line)->substrings->next;
-		i++;
+//		(*command_line)->substrings = (*command_line)->substrings->next;
+		tmp1 = tmp1->next;
+//		i++;
 	}
-	(*command_line)->substrings = tmp1;
+//	(*command_line)->substrings = tmp1;
 }
 
 
 void expand_redirections(t_substring *substring, t_native_redirection *n_redirection)
 {
+//	int	i;
 	t_expanded_redirection	*exp_redirection;
 	size_t					len_to_next_quote;
 	char					*extracted_line;
@@ -51,20 +65,21 @@ void expand_redirections(t_substring *substring, t_native_redirection *n_redirec
 	exp_redirection = NULL;
 	if (init_expanded_redirection_struct(&exp_redirection) == -1)
 		exp_redirection->alloc_succeed = false;
+//	i = 0;
 	if (n_redirection)
 	{
 		if (n_redirection->content[0] == '\'')
 		{
-			len_to_next_quote = strspn(&n_redirection->content[1], "\'");
+			len_to_next_quote = strcspn(&n_redirection->content[1], "\'");
 			extracted_line = ft_substr(&n_redirection->content[1], 0, len_to_next_quote);
-			n_redirection->content += len_to_next_quote;
+//			n_redirection->content += len_to_next_quote + 2;
 //			search_variables(extracted_line);
 		}
-		if (n_redirection->content[0] == '\"')
+		else if (n_redirection->content[0] == '\"')
 		{
-			len_to_next_quote = strspn(&n_redirection->content[1], "\"");
+			len_to_next_quote = strcspn(&n_redirection->content[1], "\"");
 			extracted_line = ft_substr(&n_redirection->content[1], 0, len_to_next_quote);
-			n_redirection->content += len_to_next_quote;
+//			n_redirection->content += len_to_next_quote + 2;
 //			search_variables(extracted_line);
 		}
 		else
@@ -81,6 +96,7 @@ void expand_redirections(t_substring *substring, t_native_redirection *n_redirec
 }
 void expand_arguments(t_substring *substring, t_native_argument *n_argument)
 {
+	int i;
 	t_expanded_argument		*exp_argument;
 	size_t					len_to_next_quote;
 	char					*extracted_line;
@@ -88,28 +104,32 @@ void expand_arguments(t_substring *substring, t_native_argument *n_argument)
 	exp_argument = NULL;
 	if (init_expanded_argument_struct(&exp_argument) == -1)
 		exp_argument->alloc_succeed = false;
-	if (n_argument)
+	i = 0;
+	while (n_argument && n_argument->content[i])
 	{
-		if (n_argument->content[0] == '\'')
+		if (n_argument->content[i] == '\'')
 		{
-			len_to_next_quote = strspn(&n_argument->content[1], "\'");
-			extracted_line = ft_substr(&n_argument->content[1], 0, len_to_next_quote);
-			n_argument->content += len_to_next_quote;
+			len_to_next_quote = strcspn(&n_argument->content[i + 1], "\'");
+			extracted_line = ft_substr(&n_argument->content[i + 1], 0, len_to_next_quote);
+
+//			n_argument->content += len_to_next_quote + 2;
 //			search_variables(extracted_line);
 		}
-		if (n_argument->content[0] == '\"')
+		else if (n_argument->content[i] == '\"')
 		{
-			len_to_next_quote = strspn(&n_argument->content[1], "\"");
-			extracted_line = ft_substr(&n_argument->content[1], 0, len_to_next_quote);
-			n_argument->content += len_to_next_quote;
+			len_to_next_quote = strcspn(&n_argument->content[i + 1], "\"");
+			extracted_line = ft_substr(&n_argument->content[i + 1], 0, len_to_next_quote);
+//			n_argument->content += len_to_next_quote + 2;
 //			search_variables(extracted_line);
 		}
 		else
 		{
-			extracted_line = n_argument->content;
+			len_to_next_quote = strcspn(&n_argument->content[i], "\"\'") - 2;
+			extracted_line = ft_substr(&n_argument->content[i], 0, len_to_next_quote + 2);
 		}
 		if (extracted_line)
 			exp_argument->content = extracted_line;
+		i += (len_to_next_quote + 2);
+		ft_lst_add_back5(&substring->exp_arguments, exp_argument);
 	}
-	ft_lst_add_back5(&substring->exp_arguments, exp_argument);
 }
