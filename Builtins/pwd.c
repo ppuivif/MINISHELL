2d436b96ef66cbf9f95929d439fdd86604de50f3
Driss Abarza <drabarza@1F4.42angouleme.fr>
2024-07-08 13:47:55 +0200

    08/07/24

--- Builtins/pwd.c
+++ Builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: drabarza <drabarza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 11:30:07 by drabarza          #+#    #+#             */
-/*   Updated: 2024/07/06 20:45:59 by drabarza         ###   ########.fr       */
+/*   Updated: 2024/07/08 13:42:45 by drabarza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ int	pwd(void)
 	char	str[PATH_MAX];
 
 	if (getcwd(str, PATH_MAX))
-		printf("%s\n", str);
+	{
+		ft_putstr_fd(str, 1);
+		write(1, "\n", 1);
+	}
 	else
 		return (errno);
 	return (1);
--- includes/minishell.h
+++ includes/minishell.h
@@ -109,7 +109,7 @@ void	error_execve_and_exit(t_exec_struct **exec_struct);
 void	error_handling(t_command_line *command_line);
 
 //Builtin
-int		exec_builtin(t_exec_struct *exec_struct, t_exec_substring *substring, char **envp_arr);
+void	exec_builtin(t_exec_struct *exec_struct, t_exec_substring *substring, char **envp_arr);
 int		echo(t_exec_argument *exec_arguments);
 int		pwd(void);
 int		exit_builting(t_exec_struct *exec_struct, t_exec_argument *exec_arguments, char **envp_arr);
