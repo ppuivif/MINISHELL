1f9c18ee29c9480abbca866207746e1715e35c26
ppuivif <papuivif@gmail.com>
2024-08-29 20:06:30 +0200

    modified:   Includes/minishell.h 	modified:   execution.c

--- execution.c
+++ execution.c
@@ -6,7 +6,7 @@
 /*   By: ppuivif <ppuivif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 06:32:59 by drabarza          #+#    #+#             */
-/*   Updated: 2024/08/29 18:36:12 by ppuivif          ###   ########.fr       */
+/*   Updated: 2024/08/29 20:04:43 by ppuivif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,13 +124,35 @@ static int	*build_pid_arr(int *pid_arr, int i)
 }
 
 
+int	fork_create(t_exec_struct **exec_struct, t_exec_substring *exec_substring, \
+int *pid_arr)
+{
+	pid_t	pid_1;
+	char **envp_arr;
+	
+	envp_arr = build_envp_arr(exec_struct);
+	pid_1 = fork();
+	if (pid_1 == -1)
+	{
+		free(pid_arr);
+		perror("error\ncreate fork failed");
+		error_fork_creation_and_exit(exec_struct);
+	}
+//	*pid_arr[i] = pid_1;
+	if (pid_1 == 0)
+		exec_child(exec_substring, envp_arr, exec_struct);
+	free_arr(envp_arr);
+	return (pid_1);
+}
+
+
+
 void substrings_execution(t_exec_struct **exec_struct)
 {
 	int		i;
 	int		*pid_arr;
-	char	**envp_arr;
-	pid_t	pid_1;
 	t_exec_substring	*cursor;
+
 	int					fd_out;
 	int					fd_in;
 	int					fd[2];
@@ -138,11 +160,11 @@ void substrings_execution(t_exec_struct **exec_struct)
 	i = 0;
 	pid_arr = NULL;
 	cursor = (*exec_struct)->exec_substrings;
+
 	fd_in = STDIN_FILENO;
 	fd[0] = STDIN_FILENO;
 	fd[1] = STDOUT_FILENO;
 
-//	while (i < (int)ft_lst_size7((*exec_struct)->exec_substrings))
 	while (cursor)
 	{
 		pid_arr = build_pid_arr(pid_arr, i);
@@ -163,32 +185,13 @@ void substrings_execution(t_exec_struct **exec_struct)
 			if (fd_out == STDOUT_FILENO) //no out redirection
 				fd_out = fd[1];
 		}
-		envp_arr = build_envp_arr(exec_struct);
-		pid_1 = fork();
-		if (pid_1 == -1)
-		{
-			free(pid_arr);
-			perror("error\ncreate fork failed");
-			error_fork_creation_and_exit(exec_struct);
-		}
-		pid_arr[i] = pid_1;
-		if (pid_1 == 0)
-		{
-/*			printf("*fd_in : %d\n", fd_in);
-			printf("fd_in : %d\n", cursor->fd_in);
-			printf("*fd_out : %d\n", *fd_out);
-			printf("fd_out : %d\n", cursor->fd_out);*/
+		cursor->fd_in = fd_in;
+		cursor->fd_out = fd_out;
+		cursor->fd[0] = fd[0];
+		cursor->fd[1] = fd[1];
+		cursor->fd[1] = fd[1];
+		pid_arr[i] = fork_create(exec_struct, cursor, pid_arr);
 
-			cursor->fd_in = fd_in;
-			cursor->fd_out = fd_out;
-			cursor->fd[0] = fd[0];
-			cursor->fd[1] = fd[1];
-			cursor->fd[1] = fd[1];
-/*			exec_child(cursor, cursor->fd_in, cursor->fd_out, envp_arr, exec_struct, \
-			pid_arr, cursor->fd);*/
-			exec_child(cursor, envp_arr, exec_struct);
-			
-		}
 		close_fd(fd_in);
 		close_fd(fd_out);
 		if (cursor != ft_lst_last7((*exec_struct)->exec_substrings))
@@ -196,7 +199,6 @@ void substrings_execution(t_exec_struct **exec_struct)
 			close_fd(fd[1]);
 			fd_in = fd[0];
 		}
-		free_arr(envp_arr);
 		cursor = cursor->next;
 		i++;
 	}
@@ -227,55 +229,6 @@ void	execution(t_exec_struct **exec_struct)
 	}
 	substrings_execution(exec_struct);
 }
-
-// void	exec_child(t_exec_substring *substring, int fd_in, int fd_out, char **envp_arr, t_exec_struct **exec_struct, int *pid_arr, int *fd)
-// {
-// 	int		exit_code;
-// 	char	*path_with_cmd;
-// 	char	**cmd_arr;
-
-// 	exit_code = 0;
-// 	path_with_cmd = ft_strdup(substring->path_with_cmd);//malloc a proteger
-// 	cmd_arr = arr_copy(substring->cmd_arr);
-// 	if (substring->exec_arguments && substring->exec_arguments->is_argument_valid == false)
-// 		exit_code = (*exec_struct)->command_line->current_exit_code;
-// 	if (fd_in == -1 || fd_out == -1)
-// 		exit_code = 1;
-// 	if (fd_in > 2)
-// 	{
-// 		dup2(fd_in, STDIN_FILENO);
-// 		close_fd(fd_in);
-// 	}
-// 	if (fd_out > 2)
-// 	{
-// 		dup2(fd_out, STDOUT_FILENO);
-// 		close_fd(fd_out);
-// 	}
-// 	close_fd(fd[0]);
-// 	close_fd(fd[1]);
-// 	free(pid_arr);
-// 	rl_clear_history();
-// 	if (substring->exec_arguments && fd_out > 0)
-// 	{
-// 		if (substring->exec_arguments->is_builtin)
-// 		{
-// 			exec_builtin(*exec_struct, substring, envp_arr); //! J'ai mis ca
-// 		}
-// 	}
-// 	free_envp_struct(&(*exec_struct)->envp_struct);//! J'ai mis ca
-// 	free_all_command_line(&(*exec_struct)->command_line);//! J'ai mis ca
-// 	free_all_exec_struct(exec_struct);//! J'ai mis ca
-// 	if (path_with_cmd && cmd_arr && cmd_arr[0] && exit_code == 0)
-// 	{
-// 		execve(path_with_cmd, cmd_arr, envp_arr);
-// 		perror("error\nexecve of a cmd failed");//to verify
-// 			//exit_code = -1 ?
-// 	}
-// 	free(path_with_cmd);
-// 	free_arr(cmd_arr);
-// 	free_arr(envp_arr);
-// 	exit(exit_code);
-// }
 
 void	exec_child(t_exec_substring *substring, char **envp_arr, \
 t_exec_struct **exec_struct)
